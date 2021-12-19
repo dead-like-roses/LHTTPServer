0e3e8d4be4b7d20965b79936b97e847869fde1a3
Lennyhell <jakub.jaroniak4@gmail.com>
2021-12-20 00:06:02 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/LHTTPServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LHTTPServer.dir/main.c.o"
+  "LHTTPServer"
+  "LHTTPServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LHTTPServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
